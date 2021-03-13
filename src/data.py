276092abea2a58503c65b552fccf3731f01453ef
stash <ratatouille.apple@gmail.com>
2021-03-14 00:47:01 +0900

    modify code

--- src/data.py
+++ src/data.py
@@ -1,7 +1,11 @@
+
+#%%
 import yfinance as yf
 import pandas as pd
 import matplotlib.pyplot as plt
+from statsmodels.tsa.arima.model import ARIMA
 
+#%%
 google = yf.Ticker("GOOG")
 
 df = google.history(period="1d", interval="1m")
@@ -23,10 +27,21 @@ y_train = y[:-offset]
 X_test = X[-offset:]
 y_test = y[-offset:]
 
-plt.plot(range(0, len(y_train)), y_train, label='Train')
-plt.plot(range(len(y_train), len(y)), y_test, label='Test')
-plt.legend()
-plt.show()
+# plt.plot(range(0, len(y_train)), y_train, label='Train')
+# plt.plot(range(len(y_train), len(y)), y_test, label='Test')
+# plt.legend()
+# plt.show()
+#%%
+model = ARIMA(y_train, order=(5, 0, 1)).fit()
+forecast = model.forecast(steps=1)[0]
 
+print(f'Real data for time 0: {y_train[len(y_train)-1]}')
+print(f'Real data for time 1: {y_test[0]}')
+print(f'Predicted data for time 1: {forecast}')
 
+#%%
+import pyopt
+import robin_stocks as robinhood
+RH_USER_EMAIL = "ratatouille.apple@gmail.com"
 
+# %%
